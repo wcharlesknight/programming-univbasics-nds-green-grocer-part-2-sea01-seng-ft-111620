a762e7d593d73b8966f0f4feec5c6e766eb5df56
Charlie Knight <williamchknight@gmail.com>
2020-11-12 18:24:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -28,7 +28,7 @@ def apply_clearance(cart)
   new_cart = [] 
   cart.each do |item|
     if item[:clearance]
-      item[:price] *= 0.8
+      item[:price] *= 0.8.round(2)
       new_cart << item
     else
       new_cart << item 
